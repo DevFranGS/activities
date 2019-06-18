b57c0fa6aaeeb7f112105f7368c021ce37b85079
Francisco Garcia Sosa <fran.garcia.sosa@gmail.com>
2019-06-18 17:44:00 +0100

    ejercicios 4 5 7

---
+++ DAW/DOR/activity7/script-antixfgs.js
@@ -0,0 +1,16 @@
+$('.alert').alert(close);
+
+var contenido=document.getElementById('contenido');
+var botonHome = document.getElementById('OpcionHome');
+botonHome.onclick= function(){
+  contenido.innerHTML = "<h3> DEFAULT PAGE </h3><p> Lorem ipsum dolor sit amet, consectetur adipisci tempor incidunt ut labore et dolore magna aliqua veniam, quis nostrud exercitation ullamcorpor s commodo consequat. Duis autem vel eum irrure esse molestiae consequat, vel illum dolore eu fugi et iusto odio dignissim qui blandit praesent luptat exceptur sint occaecat cupiditat non provident, deserunt mollit anim id est laborum et dolor fuga distinct. Nam liber tempor cum soluta nobis elige quod maxim placeat facer possim omnis volupt </p><p>  Lorem ipsum dolor si amet, consectetur adipiscing incidunt ut labore et dolore magna aliquam erat nostrud exercitation ullamcorper suscipit laboris nis duis autem vel eum irure dolor in reprehenderit i, dolore eu fugiat nulla pariatur. At vero eos et accusa praesant luptatum delenit aigue duos dolor et mole provident, simil tempor sunt in culpa qui officia de fuga. Et harumd dereud facilis est er expedit disti eligend optio congue nihil impedit doming id quod assumenda est, omnis dolor repellend. Temporibud  </p><p>  Lorem ipsum dolor si amet, consectetur adipiscing incidunt ut labore et dolore magna aliquam erat nostrud exercitation ullamcorper suscipit laboris nis duis autem vel eum irure dolor in reprehenderit i dolore eu fugiat nulla pariatur. At vero eos et accus praesant luptatum delenit aigue duos dolor et mol provident, simil tempor sunt in culpa qui officia de fuga. Et harumd dereud facilis est er expedit disti eligend oprio congue nihil impedit doming id quod assumenda est, omnis dolor repellend. Temporibud </p> ";
+}
+var botonOpcion1 = document.getElementById('Opcion1');
+botonOpcion1.onclick= function(){
+  contenido.innerHTML = "<span> This is the first content:<br/><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, nihil soluta. Error dolorum cupiditate consequuntur eius! Animi expedita quo consequuntur sequi facilis sint ut neque. Quidem, dolores. Vero, nihil quod.</p><h1>and lot more</h1>Lorem ipsum dolor sit amet, consectetur adipisci tempor incidunt ut laboreLorem ipsum dolor sit amet, consectetur adipisci tempor incidunt ut labore<h1>content</h1><h4>is</h4><h4>important</h4>";
+}
+var botonOpcion2 = document.getElementById('Opcion2');
+botonOpcion2.onclick= function(){
+  contenido.innerHTML = "<b>Lorem ipsum dolor sit amet</b>, consectetur adipisci tempor incidunt ut labore et dolore magna aliqua veniam, quis nostrud exercitation ullamcorpor s commodo consequat. Duis autem vel eum irrure esse molestiae consequat, vel illum dolore eu fugi et iusto odio dignissim qui blandit praesent luptat exceptur sint occaecat cupiditat non provident, deserunt mollit anim id est laborum et dolor fuga distinct. Nam liber tempor cum soluta nobis elige quod maxim placeat facer possim omnis volupt";
+}
+ 
